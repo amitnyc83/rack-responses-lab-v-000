9c5b15b3c4632958ca66c2bfc06c197442704748
Amit Singh <amit_nyc@live.com>
2018-09-11 19:52:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     current = Time.new
 
-    if current.hour < 13
+    if current.hour > 13
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
