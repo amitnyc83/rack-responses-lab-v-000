cd056434a6cfe7866aaaea7d5b03c9e978313bc9
Amit Singh <amit_nyc@live.com>
2018-09-11 19:32:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 class Application
 
-def call(env)
-resp = Rack::Response.new
- current = Time.new
+  def call(env)
+    resp = Rack::Response.new
+    current = Time.new
 
-if current.strftime("%H") > 0 && current.strftime("%H") < 13
-  resp.write "Good Morning"
-else
-  resp.write "Good Afternoon"
-end
+    if current.strftime("%H") > 0 && current.strftime("%H") < 13
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
 
-end
-resp.finish
+  end
+
 
 end
