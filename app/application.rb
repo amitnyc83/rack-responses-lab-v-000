a7e15e53a0556a5c38256ac97933924e934fa40e
Amit Singh <amit_nyc@live.com>
2018-09-11 20:11:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     current = Time.new
-
+binding.pry
     if  13 > current.hour
       resp.write "Good Morning"
     else
